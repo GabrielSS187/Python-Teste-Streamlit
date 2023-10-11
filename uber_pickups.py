5a3396d5f71297c59469e4d7ff7225ccf89cc547
Gabriel Silva <gabriel127.gk@gmail.com>
2023-10-10 21:52:56 -0300

    App Streamlit - OK

---
+++ uber_pickups.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Coletas do Uber em Nova Iorque")
+
+DATE_COLUMN = "date/time"
+DATA_URL = ("https://s3-us-west-2.amazonaws.com/"
+         "streamlit-demo-data/uber-raw-data-sep14.csv.gz")
+
+
+@st.cache_data
+def load_data(nrows: int):
+  data = pd.read_csv(DATA_URL, nrows=nrows)
+  lowercase = lambda x: str(x).lower()
+  data.rename(lowercase, axis="columns", inplace=True)
+  data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+  return data
+
+data_load_state = st.text("Carregando dados...")
+
+data = load_data(10000)
+
+data_load_state.text("Feito! (usando st.cache_data)")
+
+if st.checkbox("Mostrar dados brutos"):
+    st.subheader("Dados não tratados")
+    st.write(data)
+
+st.subheader("Número de coletas por hora")
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
+
+st.subheader("Mapa de todas as coletas")
+st.map(data)
+
+hour_to_filter = st.slider("hora", 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f"Mapa de todas as coletas em {hour_to_filter}:00")
+st.map(filtered_data)
